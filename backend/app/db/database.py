aff6622cc3565eb220d4bf7b075b8a8d0579357b
kumar <padari.k@northeastern.edu>
2026-09-10 15:06:25 -0400

    db connection to use env

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,8 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/workflowdb"
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@db:5432/workflowdb",
+)
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
